2d2d7eadc7ac0279a0a960bb219fa4ad58177d8d
Akash15121992 <akashkumar1528@outlook.com>
2024-04-16 14:17:53 +0530

    Exception class cleanup

--- src/main/java/dev/sandeep/EcomProductServiceDec23/exception/ProductServiceExceptionHandler.java
+++ src/main/java/dev/sandeep/EcomProductServiceDec23/exception/ProductServiceExceptionHandler.java
@@ -9,19 +9,10 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class ProductServiceExceptionHandler {
 
-    @ExceptionHandler(ProductNotFoundException.class)
-    public ResponseEntity handleProductNotFoundException(ProductNotFoundException pe){
+    @ExceptionHandler({ProductNotFoundException.class, NoProductPresentException.class})
+    public ResponseEntity handleProductNotFoundExceptionAndNoProductPresentException(ProductNotFoundException pe){
         ExceptionResponseDto exceptionResponseDto = new ExceptionResponseDto(
                 pe.getMessage(),
-                404
-        );
-        return new ResponseEntity<>(exceptionResponseDto, HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler(NoProductPresentException.class)
-    public ResponseEntity handleNoProductPresentException(NoProductPresentException ne){
-        ExceptionResponseDto exceptionResponseDto = new ExceptionResponseDto(
-                ne.getMessage(),
                 404
         );
         return new ResponseEntity<>(exceptionResponseDto, HttpStatus.NOT_FOUND);
